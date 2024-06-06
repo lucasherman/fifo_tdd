425af501c590bda458827126fc5b648d19fb2ae1
lukaszherman82 <lukasz.herman@hogarthww.com>
2024-06-07 01:16:54 +0200

    S-20 - refactor - remove size property

--- fifo.js
+++ fifo.js
@@ -1,28 +1,25 @@
 class Fifo {
   constructor() {
-    this.size = 0;
     this.stack = [];
   }
 
   isEmpty() {
-    return this.size === 0;
+    return this.stack.length === 0;
   }
 
   push(element) {
     this.stack.push(element);
-    this.size += 1;
   }
 
   pop() {
-    if (this.size <= 0) {
+    if (this.stack.length <= 0) {
       throw RangeError("Cannot pop an empty stack")
     }
-    this.size -= 1;
     return this.stack.pop();
   }
 
   getSize() {
-    return this.size;
+    return this.stack.length;
   }
 
 }
